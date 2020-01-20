1b3fd280ef9869eb09c3a75e3f67a655fcc33b47
RudarDaman <damanrudar@gmail.com>
2020-01-20 21:13:47 +0530

    Initial commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cors = require('cors');
 var bodyParser = require('body-parser');
 var path = require('path');
 // const jwt = require('_helpers/jwt');
-const errorHandler = require('./_helpers/error-handler');
+const errorHandler = require('./server/_helpers/error-handler');
 
 //Port
 const port = 3000;
@@ -17,10 +17,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/api/potato/type', require('./potato/potato-type/potato-type.controller'));
-app.use('/api/users', require('./users/user.controller'));
+app.use('/api/potato/type', require('./server/potato/potato-type/potato-type.controller'));
+app.use('/api/users', require('./server/users/user.controller'));
 
 app.use(errorHandler);
 
